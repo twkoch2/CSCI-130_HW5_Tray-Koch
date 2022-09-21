7877d00d4ba2dd3d621851b9ffae81138aad0fce
twkoch <111785868+twkoch2@users.noreply.github.com>
2022-09-21 15:24:31 +0000

    Had to move code originally written in P1 to P2.

--- HW5P1/main.cpp
+++ HW5P1/main.cpp
@@ -1,6 +1,44 @@
+/************************************************************
+Programmer: Tray Koch
+Description: Test loading a *.dat file. Follows the handout EOF_While_FileImport.pdf
+Data Modified: September 20, 2022
+************************************************************/
+
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
 
 int main() 
 {
+  //declare variables
+  string firstName;
+  string date;
+  int TestResult;
+  int count = 0;
+  int cumulative_cases = 0;
+
+  //declare stream variables
+  ifstream inFile;  //needed to access files we want to load
+  ofstream outFile;  //needed to save the results of the analysis
+
+  //open the input file
+  inFile.open("TestResultsData.dat");
+
+  //read in the date - first line of file
+  inFile >> date;
+  cout << date << endl;
+
+  //read in the 2 pieces of info on the second line
+  inFile >> firstName;
+  cout << firstName << ' ';
+  inFile >> TestResult;
+  cout << TestResult << endl;
+
+
+  
+
   
 }
