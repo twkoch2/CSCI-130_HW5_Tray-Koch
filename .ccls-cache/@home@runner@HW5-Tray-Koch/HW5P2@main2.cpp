681a8148e5f95c4bbfa0c522c26be220dfc07f25
twkoch <111785868+twkoch2@users.noreply.github.com>
2022-09-22 16:54:56 +0000

    Data imports and displays on the console.

--- .ccls-cache/@home@runner@HW5-Tray-Koch/HW5P2@main2.cpp
+++ .ccls-cache/@home@runner@HW5-Tray-Koch/HW5P2@main2.cpp
@@ -1,13 +1,30 @@
+/************************************************************
+Programmer: Tray Koch
+Description: Test loading a *.txt file. Follows the handout FileImport2.pdf This handout outlines that uses of the break function to salvage data when something goes awry.
+Data Modified: September 22, 2022
+************************************************************/
+
 #include <iostream>
-# include <fstream>
+#include <fstream>
 
 using namespace std;
 
 int main ()
 {
+  //identify variables
+  int dataTemp;
+  
   // load input file 
   ifstream inFile; //declare stream variable
   inFile.open ("NumberData1.txt");
 
+  //load all data in while loop
+  while (!inFile.eof())
+    {
+      inFile >> dataTemp;
+      cout << dataTemp << endl;
+    }
+
+  
   
 }
