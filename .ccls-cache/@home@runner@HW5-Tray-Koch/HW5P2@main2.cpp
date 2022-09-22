9e3580ff8bd46c060e132ecbb2130a2ee2502cc3
twkoch <111785868+twkoch2@users.noreply.github.com>
2022-09-22 17:01:54 +0000

    Worked through step 6 on worksheet

--- .ccls-cache/@home@runner@HW5-Tray-Koch/HW5P2@main2.cpp
+++ .ccls-cache/@home@runner@HW5-Tray-Koch/HW5P2@main2.cpp
@@ -19,10 +19,20 @@ int main ()
   inFile.open ("NumberData1.txt");
 
   //load all data in while loop
-  while (!inFile.eof())
+  while (inFile >> dataTemp)
     {
-      inFile >> dataTemp;
-      cout << dataTemp << endl;
+      if (dataTemp >= 0)
+      {
+        // update cumulative value of all data and number of data points loaded
+      }
+
+      else //the value is negative and we should exit
+      {
+        cout << "Negative value encountered of " << dataTemp << "." << endl;
+        cout << "Will not use rest of data..." << endl;
+        break;
+      }
+      //cout << dataTemp << endl;
     }
 
   
