3b71953484956b799190a4cd495bc1be23c61843
twkoch <111785868+twkoch2@users.noreply.github.com>
2022-09-22 15:07:01 +0000

    P2 Started

---
+++ .ccls-cache/@home@runner@HW5-Tray-Koch/HW5P2@main2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+# include <fstream>
+
+using namespace std;
+
+int main ()
+{
+  // load input file 
+  ifstream inFile; //declare stream variable
+  inFile.open ("NumberData1.txt");
+
+  
+}
